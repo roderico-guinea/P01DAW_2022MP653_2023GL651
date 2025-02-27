2283ef0d40e35fe9d435ec38af59f64c74aa624d
Roderico Guinea <guinearoderico2@gmail.com>
2025-02-27 17:59:58 -0600

    Creacion de Modelo Reserva

---
+++ P01_2022MP653_2023GL651/Models/Reserva.cs
@@ -0,0 +1,9 @@
+﻿public class Reserva
+{
+    public int Id { get; set; }
+    public int UsuarioId { get; set; }
+    public int EspacioId { get; set; }
+    public DateTime Fecha { get; set; }
+    public TimeSpan Hora { get; set; }
+    public int CantidadHoras { get; set; }
+}
