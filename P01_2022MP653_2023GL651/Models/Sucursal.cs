40efe10f071ed0799af1ab06acec7c734dc78adb
Roderico Guinea <guinearoderico2@gmail.com>
2025-02-27 17:49:50 -0600

    Creacion de Modelo Sucursal

---
+++ P01_2022MP653_2023GL651/Models/Sucursal.cs
@@ -0,0 +1,9 @@
+﻿public class Sucursal
+{
+    public int Id { get; set; }
+    public string? Nombre { get; set; }
+    public string? Direccion { get; set; }
+    public string? Telefono { get; set; }
+    public int AdministradorId { get; set; }
+    public int NumeroEspacios { get; set; }
+}
