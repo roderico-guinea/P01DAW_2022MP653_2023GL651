46c441436ecba998c2dcf975220d2e39b47d6f07
diego <diego@LAPTOP-0PV5A95C>
2025-02-27 17:40:30 -0600

    creacion del modelo de usuario.

---
+++ P01_2022MP653_2023GL651/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿namespace P01_2022MP653_2023GL651.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public string Telefono { get; set; }
+        public string Contraseña { get; set; }
+        public string Rol { get; set; }
+    }
+
+}
