3e4d2238feb9a605764b61af9cea4f82d05b5ab9
diego <diego@LAPTOP-0PV5A95C>
2025-02-27 17:53:24 -0600

    creacion de el modelo espacio parqueo

---
+++ P01_2022MP653_2023GL651/Models/EspacioParqueo.cs
@@ -0,0 +1,12 @@
+﻿namespace P01_2022MP653_2023GL651.Models
+{
+    public class EspacioParqueo
+    {
+        public int Id { get; set; }
+        public int SucursalId { get; set; }
+        public int Numero { get; set; }
+        public string Ubicacion { get; set; }
+        public decimal CostoPorHora { get; set; }
+        public string Estado { get; set; }
+    }
+}
