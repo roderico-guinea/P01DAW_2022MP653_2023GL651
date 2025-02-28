4c8a339f3135b69b807e1f35fa4e6635752de73b
diego <diego@LAPTOP-0PV5A95C>
2025-02-27 19:57:07 -0600

    cambios menores

--- P01_2022MP653_2023GL651/Models/Reserva.cs
+++ P01_2022MP653_2023GL651/Models/Reserva.cs
@@ -1,9 +1,12 @@
-﻿public class Reserva
+﻿namespace P01_2022MP653_2023GL651.Models
 {
-    public int Id { get; set; }
-    public int UsuarioId { get; set; }
-    public int EspacioId { get; set; }
-    public DateTime Fecha { get; set; }
-    public TimeSpan Hora { get; set; }
-    public int CantidadHoras { get; set; }
+    public class Reserva
+    {
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public int EspacioId { get; set; }
+        public DateTime Fecha { get; set; }
+        public TimeSpan Hora { get; set; }
+        public int CantidadHoras { get; set; }
+    }
 }
--- P01_2022MP653_2023GL651/Models/Sucursal.cs
+++ P01_2022MP653_2023GL651/Models/Sucursal.cs
@@ -1,9 +1,12 @@
-﻿public class Sucursal
+﻿namespace P01_2022MP653_2023GL651.Models
 {
-    public int Id { get; set; }
-    public string? Nombre { get; set; }
-    public string? Direccion { get; set; }
-    public string? Telefono { get; set; }
-    public int AdministradorId { get; set; }
-    public int NumeroEspacios { get; set; }
+    public class Sucursal
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Direccion { get; set; }
+        public string? Telefono { get; set; }
+        public int AdministradorId { get; set; }
+        public int NumeroEspacios { get; set; }
+    }
 }
