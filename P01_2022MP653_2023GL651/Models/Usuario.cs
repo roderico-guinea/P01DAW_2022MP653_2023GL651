dcfc0d32bb4f1b478bcaa9c54aae12d4b1575e17
Roderico Guinea <guinearoderico2@gmail.com>
2025-02-27 18:21:20 -0600

    Instalkacion de Paquetes Nuget EntityFramwork y Correcion de errores en la clase usuarios

--- P01_2022MP653_2023GL651/Models/Usuario.cs
+++ P01_2022MP653_2023GL651/Models/Usuario.cs
@@ -3,11 +3,11 @@
     public class Usuario
     {
         public int Id { get; set; }
-        public string Nombre { get; set; }
-        public string Correo { get; set; }
-        public string Telefono { get; set; }
-        public string Contraseña { get; set; }
-        public string Rol { get; set; }
+        public string? Nombre { get; set; }
+        public string? Correo { get; set; }
+        public string? Telefono { get; set; }
+        public string? Contraseña { get; set; }
+        public string? Rol { get; set; }
     }
 
 }
